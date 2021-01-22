16f6cf5f9764987d1cfe16b3dcfa7041bb30a7e9
lemuel_taeza <lemuel.taeza@oracle.com>
2021-01-22 21:50:52 +0800

    debug

--- src/main/resources/src/app/people/person-form/children-form/children-form.component.ts
+++ src/main/resources/src/app/people/person-form/children-form/children-form.component.ts
@@ -17,42 +17,50 @@ import { debounceTime, distinctUntilChanged, map, startWith, switchMap } from 'r
 })
 export class ChildrenFormComponent implements OnInit {
 
-  @Input()  myForm: FormGroup;
+  @Input() myForm: FormGroup;
   @Input() person: Person;
   namesListControl = new FormControl();
-  
+
   filteredOptions: Observable<Name[]>;
-  constructor(    public dialog: MatDialog, private peopleService: PeopleService) { }
+  constructor(public dialog: MatDialog, private peopleService: PeopleService) { }
 
   ngOnInit(): void {
     this.filteredOptions = this.namesListControl.valueChanges
-    .pipe(
-      startWith(''),
-      debounceTime(400),
-      distinctUntilChanged(),
-      switchMap(val => {
-        return this.filter(val || '')
-      })
-    );
+      .pipe(
+        startWith(''),
+        debounceTime(400),
+        distinctUntilChanged(),
+        switchMap(val => {
+          return this.filter(val || '')
+        })
+      );
   }
 
-   // filter and return the values
-   filter(val: string): Observable<any[]> {
+  // filter and return the values
+  filter(val: string): Observable<any[]> {
     // call the service which makes the http-request
     return of(this.peopleService.allPeopleCache)
       .pipe(
-        map(response => response.filter(person => {
-          console.log("cache", this.peopleService.allPeopleCache)
-          let childrenArray =  this.myForm.get("children").value;
-          let arr = childrenArray ? childrenArray : [];     
-          //don't show already added
-          const isInArray = arr.find(x => x.firstName === person.firstName && x.lastName === person.lastName);
-          //don't show the person being edited
-          const notSelf: boolean = this.person.firstName !== person.firstName && this.person.lastName !== person.lastName;
-          return !isInArray && notSelf && (person.firstName.toLowerCase().indexOf(val.toLowerCase()) === 0
-            || person.lastName.toLowerCase().indexOf(val.toLowerCase()) === 0
-            || person.nickname.toLowerCase().indexOf(val.toLowerCase()) === 0)
-        }))
+        map(peepsArray => {
+          let childrenArray = this.myForm.get("children").value;
+
+          let filtered: PersonSimple[] =  peepsArray.filter(person => {
+
+            let arr = childrenArray ? childrenArray : [];
+            //don't show already added
+            const isInArray = arr.find(x => x.firstName === person.firstName && x.lastName === person.lastName);
+            //don't show the person being edited
+            const notSelf: boolean = this.person.firstName !== person.firstName && this.person.lastName !== person.lastName;
+            
+            return !isInArray && notSelf && (person.firstName.toLowerCase().indexOf(val.toLowerCase()) === 0
+              || person.lastName.toLowerCase().indexOf(val.toLowerCase()) === 0
+              || person.nickname.toLowerCase().indexOf(val.toLowerCase()) === 0)
+          })
+
+          console.log("filtered", filtered)
+
+          return filtered;
+        })
       )
   }
 
@@ -65,7 +73,7 @@ export class ChildrenFormComponent implements OnInit {
     dialogRef.afterClosed().subscribe(result => {
       console.log('The dialog was closed');
       console.log("result", result);
-      if (result != null){
+      if (result != null) {
         this.addChild(result);
       }
     });
